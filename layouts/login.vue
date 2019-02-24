e8b5c01fbb52105ea5b828afdadd3c4381d77a96
Naren <developer.naren@gmail.com>
2019-02-24 22:08:57 +0400

    fixed listing page

--- layouts/login.vue
+++ layouts/login.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="container">
+  <section class="login-container">
     <div>
       <nuxt/>
     </div>
@@ -22,34 +22,12 @@
     @apply min-h-screen flex justify-center items-center text-center mx-auto;
   }
   */
-  .container {
+  .login-container {
     margin: 0 auto;
     min-height: 100vh;
     display: flex;
     justify-content: center;
     align-items: center;
     text-align: center;
-  }
-
-  .title {
-    font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-    display: block;
-    font-weight: 300;
-    font-size: 100px;
-    color: #35495e;
-    letter-spacing: 1px;
-  }
-
-  .subtitle {
-    font-weight: 300;
-    font-size: 42px;
-    color: #526488;
-    word-spacing: 5px;
-    padding-bottom: 15px;
-  }
-
-  .links {
-    padding-top: 15px;
   }
 </style>
